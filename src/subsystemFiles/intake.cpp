f00d0bd4c0d19a1f77928775acc777637bb17404
Senor Orange <joshmoller7@gmail.com>
2024-08-20 19:20:01 -0700

    2024-08-20
    Intake code updated to now work. Altered the tutorial code with the new pros library

--- src/subsystemFiles/intake.cpp
+++ src/subsystemFiles/intake.cpp
@@ -1,13 +1,12 @@
 #include "main.h"
+#include "pros/motors.hpp"
 
-//HELPER FUNCTIONS
-void setIntake(int power) {
-    ;
-}
+
 
 //DRIVER CONTROL FUNCTIONS
 void setIntakeMotors() {
     //bottom trigger intakes, top trigger outtakes
-    int intakePower = 127 * (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1) - controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2));
-    setIntake(intakePower);
+    int intakeVelocity = 200 * (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1) - controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2));
+    intake.move_velocity(intakeVelocity);
+    
 }
