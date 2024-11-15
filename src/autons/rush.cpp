7fc3061b1bf83c193c769c9bb11d48cd3b914551
SenorOrange <136025535+SenorOrange@users.noreply.github.com>
2024-11-14 19:52:22 -0800

    2024-11-14

--- src/autons/rush.cpp
+++ src/autons/rush.cpp
@@ -9,25 +9,26 @@
 
 void rush() {
     chassis.setPose(0, 0, 0);
-    chassis.moveToPoint(0, -42, 1000, {.forwards=false});
-    chassis.turnToHeading(30, 1000);
-    chassis.moveToPose(flip * -13, -60, 30, 1000, {.forwards=false});
+    //chassis.moveToPoint(0, -32, 1000, {.forwards=false});
+    chassis.moveToPose(flip * -41, -85, 45, 1000, {.forwards=false});
     chassis.waitUntilDone();
-    clamp();
-    spinIntake();
-    chassis.moveToPose(flip * -15, -40, 110, 1000);
     pros::delay(1000);
-    chassis.waitUntilDone();
-    unclamp();
-    chassis.moveToPose(flip * -25, -0, 45, 1000);
-    pros::delay(500);
-    stopIntake();
-    chassis.moveToPose(flip * 15, -40, 60, 1000, {.forwards=false});
-    chassis.waitUntilDone();
     clamp();
     spinIntake();
-    raiseLadyBrown();
-    chassis.moveToPose(flip * -60, 60, 90, 1000, {.forwards=false});
-    stopIntake();
+    
+    //chassis.moveToPose(flip * -42, -20, 90, 1000);
+    //pros::delay(1000);
+    //chassis.waitUntilDone();
+    //unclamp();
+    //chassis.moveToPose(flip * -25, -0, 45, 1000);
+    //pros::delay(500);
+    //stopIntake();
+    //chassis.moveToPose(flip * 15, -40, 60, 1000, {.forwards=false});
+    //chassis.waitUntilDone();
+    //clamp();
+    //spinIntake();
+    //raiseLadyBrown();
+    //chassis.moveToPose(flip * -60, 60, 90, 1000, {.forwards=false});
+    //stopIntake();
 }
  
