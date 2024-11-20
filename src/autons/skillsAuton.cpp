727b89bb3f005fb8b278eaea77a42cc4ec044c96
Senor Orange <joshmoller7@gmail.com>
2024-11-19 23:03:41 -0800

    2024-11-19 v2

--- src/autons/skillsAuton.cpp
+++ src/autons/skillsAuton.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
+#include "pros/rtos.hpp"
 #include "subsystemHeaders/globals.hpp"
 #include "subsystemHeaders/intake.hpp"
 #include "subsystemHeaders/mogoMech.hpp"
@@ -7,35 +8,98 @@
 #include "subsystemHeaders/poos.hpp"
 
 
-//Set of Functions to put ring in lady brown mech
-//ladyBrownPrime();
-//spinIntake();
-//pros::delay(1000);
-//reverseIntake();
-//pros::delay(100);
-//stopIntake();
-
 
 void skills() {
+
+    //Red Positive Corner
     chassis.setBrakeMode(MOTOR_BRAKE_BRAKE);
     chassis.setPose(72, 16, 180);
-    chassis.turnToHeading(-90, 2000);
-    chassis.moveToPoint(96, 24, 2000, {.forwards=false});
+    chassis.moveToPoint(72, 24, 2000, {.maxSpeed=150});
+    chassis.turnToHeading(-90, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(96, 24, 2000, {.forwards=false, .maxSpeed=150});
     chassis.waitUntilDone();
     clamp();
     spinIntake();
-    chassis.turnToHeading(0, 2000);
-    chassis.moveToPoint(96, 48, 2000);
-    chassis.turnToHeading(90, 2000);
-    chassis.moveToPoint(120, 48, 2000);
-    chassis.turnToHeading(180, 2000);
-    pros::delay(2000);
-    chassis.moveToPoint(120, 17, 2000);
-    chassis.turnToHeading(45, 2000);
-    chassis.moveToPoint(130, 24, 2000);
-    chassis.moveToPoint(136, 16, 2000, {.forwards=false});
+    chassis.turnToHeading(0, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(96, 48, 2000, {.maxSpeed=150});
+    chassis.turnToHeading(90, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(120, 48, 2000, {.maxSpeed=150});
+    chassis.turnToHeading(180, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(120, 17, 2000, {.maxSpeed=150});
+    chassis.turnToHeading(45, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(130, 24, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(130, 14, 2000, {.forwards=false, .maxSpeed=150});
+    chassis.turnToHeading(-45, 2000, {.maxSpeed=150});
     chassis.waitUntilDone();
     unclamp();
     stopIntake();
+
+    //Reset Position
+    chassis.moveToPoint(130, 24, 2000, {.forwards=false, .maxSpeed=150});
+    chassis.turnToHeading(90, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(144, 24, 2000, {.maxSpeed=150});
+    chassis.setPose(138, 24, 90);
+
+    //Red Negative Corner
+    chassis.moveToPoint(48, 24, 2000, {.forwards=false, .maxSpeed=150});
+    chassis.waitUntilDone();
+    clamp();
+    spinIntake();
+    chassis.turnToHeading(0, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(48, 48, 2000, {.maxSpeed=150});
+    chassis.turnToHeading(-90, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(24, 48, 2000, {.maxSpeed=150});
+    chassis.turnToHeading(-25, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(12, 72, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(24, 48, 2000, {.forwards=false, .maxSpeed=150});
+    chassis.turnToHeading(180, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(24, 17, 2000, {.maxSpeed=150});
+    chassis.turnToHeading(45, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(14, 24, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(14, 14, 2000, {.forwards=false, .maxSpeed=150});
+    chassis.turnToHeading(45, 2000, {.maxSpeed=150});
+    chassis.waitUntilDone();
+    unclamp();
+    stopIntake();
+
+    //Reset Position
+    chassis.moveToPoint(24, 24, 2000, {.forwards=false, .maxSpeed=150});
+    chassis.turnToHeading(180, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(24, 0, 2000, {.maxSpeed=150});
+    chassis.setPose(24, 16, 180);
+
+    //Blue Negative Corner
+    spinIntake();
+    chassis.moveToPoint(24, 96, 2000, {.maxSpeed=150});
+    stopIntake();
+    chassis.turnToHeading(90, 2000, {.maxSpeed=150});
+    spinIntake();
+    chassis.moveToPoint(48, 96, 2000, {.maxSpeed=150});
+    stopIntake();
+    chassis.turnToHeading(33, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(60, 132, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(0, 144, 2000, {.maxSpeed=150});
+
+    //Reset Position
+    chassis.moveToPoint(24, 120, 2000, {.forwards=false, .maxSpeed=150});
+    chassis.turnToHeading(-90, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(0, 120, 2000, {.maxSpeed=150});
+    chassis.setPose(6, 120, 90);
+
+    //Blue Positive Corner
+    chassis.moveToPoint(72, 120, 2000, {.forwards=false, .maxSpeed=150});
+    chassis.waitUntilDone();
+    clamp();
+    spinIntake();
+    pros::delay(5000);
+    chassis.turnToHeading(135, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(96, 96, 2000, {.maxSpeed=150});
+    chassis.turnToHeading(45, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(120, 96, 2000, {.maxSpeed=150});
+    chassis.turnToHeading(25, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(132, 72, 2000, {.maxSpeed=150});
+    chassis.turnToHeading(0, 2000, {.maxSpeed=150});
+    chassis.moveToPoint(132, 120, 2000, {.maxSpeed=150});
+
 
 }
